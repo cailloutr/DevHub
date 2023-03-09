d57859ca9dc9bbd1aa9f5087ca07895c4244847f
cailloutr <caio.trocilo@gmail.com>
2023-03-09 02:03:29 -0300

    Cria a tela de perfil do usuário

--- app/src/main/java/com/cailloutr/devhub/MainActivity.kt
+++ app/src/main/java/com/cailloutr/devhub/MainActivity.kt
@@ -3,13 +3,23 @@ package com.cailloutr.devhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cailloutr.devhub.model.User
 import com.cailloutr.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp(modifier = Modifier.fillMaxSize())
                 }
             }
         }
@@ -30,14 +40,73 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(modifier: Modifier = Modifier) {
+    Surface(modifier = modifier) {
+        Profile(
+            user = User(
+                name = "Caio Trócilo Canazarro",
+                user = "cailloutr",
+                bio = "Estudante de Sistemas de Informação IFF - Fluminense, Itaperuna - RJ",
+                image = R.drawable.profile_picture,
+                pronouns = "he/him"
+            )
+        )
+    }
+}
+
+@Composable
+fun Profile(user: User) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row() {
+            Image(
+                painter = painterResource(id = user.image),
+                contentDescription = stringResource(
+                    R.string.profile_picture
+                ),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(CircleShape)
+                    .border(
+                        BorderStroke(2.dp, MaterialTheme.colorScheme.secondary),
+                        CircleShape
+                    )
+            )
+        }
+
+        Row {
+            Text(
+                text =
+                user.name,
+                modifier = Modifier.padding(vertical = 4.dp),
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+        Row {
+            Text(
+                text = "${user.user} - ${user.pronouns}",
+                modifier = Modifier.padding(vertical = 4.dp),
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+        Row {
+            Text(
+                text = user.bio,
+                modifier = Modifier.padding(vertical = 4.dp),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DevHubTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
---
+++ app/src/main/java/com/cailloutr/devhub/model/User.kt
@@ -0,0 +1,10 @@
+package com.cailloutr.devhub.model
+
+data class User(
+    val name: String = "",
+    val user: String = "",
+    val bio: String = "",
+    val image: Int = 0,
+    val pronouns: String = ""
+) {
+}
